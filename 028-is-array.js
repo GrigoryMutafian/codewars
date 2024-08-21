9e507c7b7348c53db5004786f98a076f9bd77c06
GrigoryMutafian <gmutafan@gmail.com>
2024-08-21 20:08:58 +0300

    exercises

--- 028-is-array.js
+++ 028-is-array.js
@@ -1,3 +1,7 @@
 function arrCheck(arr) {
   return arr.every((item) => Array.isArray(item));
 }
+
+console.log(arrCheck([["string"]]), true);
+console.log(arrCheck([[1], [2], [3]]), true);
+console.log(arrCheck(["A", "R", "R", "A", "Y"]), false);
