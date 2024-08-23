5dcb4c1727b8a4b34aa2caa2f270353ec67a14c1
GrigoryMutafian <gmutafan@gmail.com>
2024-08-23 15:55:28 +0300

    fix

--- 026-scrolling-text.js
+++ 026-scrolling-text.js
@@ -1,17 +1,8 @@
 function scrollingText(text) {
-  let currentStr = text.split("");
-  let result = [text.toUpperCase()];
+  let result = [];
 
-  for (let i = 0; i < text.length - 1; i++) {
-    currentStr.push(currentStr[0]);
-    let arr = [];
-
-    for (let y = 1; y < currentStr.length; y++) {
-      arr.push(currentStr[y]);
-    }
-
-    result.push(arr.join("").toUpperCase());
-    currentStr = arr;
+  for (var i = 0; i < text.length; i++) {
+    result.push((text.slice(i) + text.slice(0, i)).toUpperCase());
   }
 
   return result;
